6068f80f63350fc7bb35d136eb0a64a6e9d6fb6f
Lev Karpov <nt66@bk.ru>
2020-09-29 22:25:47 +0300

    Заменяет конкатенацию строк

--- js/main.js
+++ js/main.js
@@ -9,11 +9,11 @@ const MIN_NUMBER_AVATAR = 1;
 const MAX_NUMBER_AVATAR = 6;
 
 /* массив комментариев*/
-let DESCRIPTION = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+const DESCRIPTION = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
   `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`, `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
   `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`];
 /* имя автора комента */
-let NAME_AUTHOR = [`Лев`, `Александр`, `Игорь`, `Даниил`, `Владимир`, `Антон`, `Михаил`, `Екатерина`, `Варвара`, `София`];
+const NAME_AUTHOR = [`Лев`, `Александр`, `Игорь`, `Даниил`, `Владимир`, `Антон`, `Михаил`, `Екатерина`, `Варвара`, `София`];
 
 let similarListElement = document.querySelector(`.pictures`);
 let similarPictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
@@ -45,7 +45,7 @@ for (let j = LIKE_START; j <= LIKE_END; j++) {
 let comments = [];
 for (let t = 0; t < COUNT_OF_COMMENTS; t++) {
   let randomComment = {
-    avatar: `img/avatar-` + numberAvatar[getRandomElement(numberAvatar)] + `.svg`,
+    avatar: `img/avatar-${numberAvatar[getRandomElement(numberAvatar)]}.svg`,
     name: NAME_AUTHOR[getRandomElement(NAME_AUTHOR)],
     message: DESCRIPTION[getRandomElement(DESCRIPTION)]
   };
@@ -56,7 +56,7 @@ for (let t = 0; t < COUNT_OF_COMMENTS; t++) {
 let pictures = [];
 for (let k = 0; k < LENGTH_ARR_PICTURE; k++) {
   let randomPicture = {
-    url: `photos/` + numberPhoto[k] + `.jpg`,
+    url: `photos/${numberPhoto[k]}.jpg`,
     like: likes[getRandomElement(likes)],
     description: `описание фото`,
     comment: comments
