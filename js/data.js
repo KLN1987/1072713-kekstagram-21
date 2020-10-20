32f9b0a13c831f65f624ff33a50a72faef39d90c
Lev Karpov <nt66@bk.ru>
2020-10-20 13:39:33 +0300

    Добавляет разбиение на модули

---
+++ js/data.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function () {
+
+  const NUMBER_LAST_PHOTO = 25;
+  const NUMBER_FIRST_AVATAR = 1;
+  const NUMBER_LAST_AVATAR = 6;
+  const LIKE_START = 15;
+  const LIKE_END = 200;
+  const COUNT_OF_START_COMMENTS = 5;
+  const COUNT_OF_END_COMMENTS = 150;
+  const COMMETS = [`Всё отлично!`, `В целом всё неплохо. Но не всё.`, `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+    `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`, `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+    `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+  ];
+
+  const NAME_AUTHORS = [`Лев`, `Александр`, `Игорь`, `Даниил`, `Владимир`, `Антон`, `Михаил`, `Екатерина`, `Варвара`, `София`];
+
+  const DESCRIPTIONS = [`Мое лето`, `Такая милота`, `Лучшее, что я видел на земле`, `Ммм...Классно!`, `Всё отлично!`, `Люблю свою жизнь!`, `Давайте просто улыбаться!`, `Мечты сбываются!`];
+
+  const getRandomElement = function (arr) {
+    return Math.floor(Math.random() * arr.length);
+  };
+
+  const getRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+
+  const getRandomComments = function () {
+    const comments = [];
+    const randomCount = getRandomNumber(COUNT_OF_START_COMMENTS, COUNT_OF_END_COMMENTS);
+    for (let i = 0; i < randomCount; i++) {
+      const comment = {
+        avatar: `img/avatar-` + getRandomNumber(NUMBER_FIRST_AVATAR, NUMBER_LAST_AVATAR) + `.svg`,
+        message: COMMETS[getRandomElement(COMMETS)],
+        name: NAME_AUTHORS[getRandomElement(NAME_AUTHORS)]
+      };
+      comments.push(comment);
+    }
+    return comments;
+  };
+
+  const pictures = [];
+  const generateTestData = function () {
+    for (let j = 1; j <= NUMBER_LAST_PHOTO; j++) {
+      const picture = {
+        photo: `photos/${j}.jpg`,
+        likes: getRandomNumber(LIKE_START, LIKE_END),
+        comments: getRandomComments(),
+        description: DESCRIPTIONS[getRandomElement(DESCRIPTIONS)]
+      };
+      pictures.push(picture);
+    }
+  };
+
+  generateTestData();
+
+  window.data = {
+    pictures: pictures
+  };
+})();
---
+++ js/util.js
@@ -0,0 +1,51 @@
+'use strict';
+
+(function () {
+  const ESC_KEY = `Escape`;
+  const uploadFile = document.querySelector(`#upload-file`);
+  const uploadCancel = document.querySelector(`#upload-cancel`);
+  const uploadOverlay = document.querySelector(`.img-upload__overlay`);
+  const textDescription = document.querySelector(`.text__description`);
+  const imgForEffect = document.querySelector(`img`);
+  const imgUploadEffectLevel = document.querySelector(`.img-upload__effect-level`);
+
+  const onPopupEscPress = function (evt) {
+    if (evt.key !== ESC_KEY || textDescription === document.activeElement) {
+      /* проверка, есть ли курсор в поле ввода */
+      uploadOverlay.classList.remove(`hidden`);
+    } else {
+      closePopup();
+    }
+  };
+
+  /* функция открытия закрытого окна */
+  const openPopup = function () {
+    uploadOverlay.classList.remove(`hidden`);
+    document.addEventListener(`keydown`, onPopupEscPress);
+    document.querySelector(`body`).classList.add(`modal-open`);
+    imgUploadEffectLevel.classList.add(`hidden`);
+  };
+
+  /* функция закрытия открытого окна */
+  const closePopup = function () {
+    uploadOverlay.classList.add(`hidden`);
+    document.querySelector(`body`).classList.remove(`modal-open`);
+    imgForEffect.removeAttribute(`class`);
+    imgForEffect.removeAttribute(`style`);
+    imgForEffect.classList.add(`effect-none`);
+    document.removeEventListener(`keydown`, onPopupEscPress);
+  };
+
+  uploadFile.addEventListener(`change`, function () {
+    openPopup();
+  });
+
+  uploadCancel.addEventListener(`click`, function () {
+    closePopup();
+  });
+
+  window.util = {
+    ESC_KEYCODE: ESC_KEY
+  };
+
+})();
