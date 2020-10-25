5a99909f321c008c4bfa4ac20e2a3ca76ba13552
Lev Karpov <nt66@bk.ru>
2020-10-25 11:32:27 +0300

    Добавляет список фотографий из данных с сервера

--- js/bigPhoto.js
+++ js/bigPhoto.js
@@ -1,6 +1,7 @@
 "use strict";
 
 (function () {
+  const MAX_PHOTO_COUNT = 25;
   const bigPicture = document.querySelector(`.big-picture`);
   const socialCommentTemplate = document.querySelector(`.social__comments`);
   const socialFooterText = document.querySelector(`.social__footer-text`);
@@ -13,7 +14,7 @@
   const renderSinglePictures = function (item) {
     const pictureElement = similarPictureTemplate.cloneNode(true);
 
-    pictureElement.querySelector(`.picture__img`).src = item.photo;
+    pictureElement.querySelector(`.picture__img`).src = item.url;
     pictureElement.querySelector(`.picture__img`).alt = item.description;
     pictureElement.querySelector(`.picture__likes`).textContent = item.likes;
     pictureElement.querySelector(`.picture__comments`).textContent = item.comments.length;
@@ -21,43 +22,63 @@
     return pictureElement;
   };
 
-  const renderListPictures = function (arr) {
-    const fragment = document.createDocumentFragment();
-    arr.forEach(function (item) {
-      fragment.appendChild(renderSinglePictures(item));
-    });
-    similarListElement.appendChild(fragment);
-  };
-
-  renderListPictures(window.data.pictures);
-
   const getSocialComment = function (item) {
     const socialCommentElement = socialComment.cloneNode(true);
-    socialCommentElement.querySelector(`.social__picture`).src = item.avatar;
-    socialCommentElement.querySelector(`.social__picture`).alt = item.name;
-    socialCommentElement.querySelector(`p`).textContent = item.message;
+
+    socialCommentElement.querySelector(`.social__picture`).src = item.comments[0].avatar;
+    socialCommentElement.querySelector(`.social__picture`).alt = item.comments[0].name;
+    socialCommentElement.querySelector(`p`).textContent = item.comments[0].message;
 
     return socialCommentElement;
   };
+
+  const successHandler = function (photo) {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < MAX_PHOTO_COUNT; i++) {
+      fragment.appendChild(renderSinglePictures(photo[i]));
+    }
+
+    similarListElement.appendChild(fragment);
+  };
+
+  const errorHandler = function (errorMessage) {
+    const node = document.createElement(`div`);
+    node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: red;`;
+    node.style.position = `absolute`;
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = `30px`;
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement(`afterbegin`, node);
+  };
+
+  window.load(successHandler, errorHandler);
+
+  const successSocialCommets = function (photo) {
+    const fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < MAX_PHOTO_COUNT; i++) {
+      fragment.appendChild(getSocialComment(photo[i]));
+    }
+    socialCommentTemplate.innerHTML = ``;
+    socialCommentTemplate.appendChild(fragment);
+    // userDialog.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  window.load(successSocialCommets, errorHandler);
 
   const getOpenBigPhoto = function (item) {
     bigPicture.classList.remove(`hidden`);
 
     const bigPictureImg = bigPicture.querySelector(`.big-picture__img`).querySelector(`img`);
 
-    bigPictureImg.src = item.photo;
+    bigPictureImg.src = item.url;
     bigPicture.querySelector(`.likes-count`).textContent = item.likes;
     bigPicture.querySelector(`.comments-count`).textContent = item.comments.length;
 
-    const fragment = document.createDocumentFragment();
-
-    item.comments.forEach(function (i) {
-      fragment.appendChild(getSocialComment(i));
-    });
-
-    socialCommentTemplate.innerHTML = ``;
-
-    socialCommentTemplate.appendChild(fragment);
+    window.load(successSocialCommets, errorHandler);
 
     bigPicture.querySelector(`.social__caption`).textContent = item.description;
 
@@ -73,7 +94,7 @@
   // Запускаем функцию открытия большой фотографии
   const smallPictures = document.querySelectorAll(`.picture`);
   for (let i = 0; i < smallPictures.length; i++) {
-    showBigPicture(smallPictures[i], window.data.pictures[i]);
+    showBigPicture(smallPictures[i], window.load(successSocialCommets, errorHandler));
   }
 
   closeBigPicture.addEventListener(`click`, function () {
@@ -91,5 +112,4 @@
   };
 
   document.addEventListener(`keydown`, onEscClose);
-
 })();
